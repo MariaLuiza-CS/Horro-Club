95a15159871653c169d72ad0dbcabc9d300d9398
MariaLuiza-CS <m.luiza1843@gmail.com>
2022-12-26 21:32:25 -0300

    creating webview screen

--- app/src/main/java/com/example/horrorclubapp/presentation/utils/views/WebViewVideo.kt
+++ app/src/main/java/com/example/horrorclubapp/presentation/utils/views/WebViewVideo.kt
@@ -1,4 +1,23 @@
 package com.example.horrorclubapp.presentation.utils.views
 
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
 fun WebViewVideo(url:String) {
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            webViewClient = WebViewClient()
+            loadUrl(url)
+        }
+    }, update = {
+        it.loadUrl(url)
+    })
 }
